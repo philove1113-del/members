1a68354454f563b49f8458e24fc04e5db97e11e6
philove1113-del <phil.ove.1113@brssd.org>
2026-05-13 07:21:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-REDIRECT_URI = "https://doughauth.store"
+REDIRECT_URI = "https://api.doughauth.store"
 ENCODED_REDIRECT_URI = quote(REDIRECT_URI, safe="")
 
 RESTOCK_CHANNEL_ID = 1502766892186861568
